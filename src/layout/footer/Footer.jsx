6df49e27132c5dd7d333e16dc6ffb804db97925f
Manishsh221998 <manish.sh0222@gmail.com>
2024-10-01 01:31:24 +0530

    footer updated

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer footer-center bg-base-300 text-base-content p-4">
   <aside>
-    <p>Copyright © {new Date().getFullYear()} - All right reserved by Crypto</p>
+    <p>Copyright © {new Date().getFullYear()} - All right reserved by Crypto || Made with &heart; by Manish</p>
   </aside>
 </footer>
   )
