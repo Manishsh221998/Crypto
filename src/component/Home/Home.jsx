f4214b3e195bcd02eab2c021a47714612cf331b0
Manishsh221998 <manish.sh0222@gmail.com>
2024-10-01 01:41:34 +0530

    update home page

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -10,8 +10,8 @@ const Home = () => {
       src="https://academy.education.investing.com/wp-content/uploads/2022/03/bitcoin-what-is-crypto-scaled.jpg"
       className="max-w-sm h-[400px] rounded-lg shadow-2xl hover:rotate-6 hover:shadow-[#ffee8d] sm:h-[110px] sm:w-[100px] lg:h-[400px] lg:w-[420px]" />
     <div>
-      <h1 className="text-5xl py-2 font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#080808] to-[#ffe346]">Start Investing in Crypto!</h1>
-      <p className="py-6">
+      <h1 className="text-5xl lg:py-2 sm:py-0 font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#080808] to-[#ffe346]">Start Investing in Crypto!</h1>
+      <p className="lg:py-6 sm:py-0">
         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
         quasi. In deleniti eaque aut repudiandae et a id nisi.
 
--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="footer footer-center bg-base-300 text-base-content p-4">
+    <footer className="footer footer-center bg-base-300 text-base-content lg:p-1 sm:p-0">
   <aside>
     <p>Copyright © {new Date().getFullYear()} - All right reserved by Crypto || Made with <span className='text-[red]'>&hearts;</span> by Manish</p>
   </aside>
